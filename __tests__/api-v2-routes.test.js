7450137ce36e5b37390fa5333ad3bfc059458c1f
mohammad-qethama <mhmd.a@protonmail.com>
2021-05-27 00:53:28 +0300

    test 4

--- __tests__/api-v2-routes.test.js
+++ __tests__/api-v2-routes.test.js
@@ -35,8 +35,8 @@ let shirt = {
     size:'medium'
    };
 
-  let idFood;
-  let idClothes;
+  let idFood=[];
+  let idClothes=[];
 
   const tokens = {};
 
@@ -87,8 +87,8 @@ let shirt = {
 
 
 
-    idFood = responseFood.body._id;
-    idClothes = responseClothes.body._id;
+    idFood = push(responseFood.body._id);
+    idClothes = (responseClothes.body._id);
     // console.log({idFood});
     });
 
@@ -119,8 +119,8 @@ let shirt = {
 
     it('should read the food/clothes item by id',async ()=>{
 
-      const responseFood = await request.get(`/api/v2/food/${idFood}`).set('Authorization', 'Bearer ' + tokens[user.username]);
-      const responseClothes = await request.get(`/api/v2/clothes/${idClothes}`).set('Authorization', 'Bearer ' + tokens[user.username]);
+      const responseFood = await request.get(`/api/v2/food/${idFood[0]}`).set('Authorization', 'Bearer ' + tokens[user.username]);
+      const responseClothes = await request.get(`/api/v2/clothes/${idClothes[0]}`).set('Authorization', 'Bearer ' + tokens[user.username]);
     
       expect(responseFood.status).toEqual(200);
       expect(responseClothes.status).toEqual(200);
@@ -154,8 +154,8 @@ let shirt = {
         size:'large'
     }
     //act
-    let responseFood = await request.put(`/api/v2/food/${idFood}`).send(editFood).set('Authorization', 'Bearer ' + tokens[user.username]);
-    let responseClothes = await request.put(`/api/v2/clothes/${idClothes}`).send(editShirt).set('Authorization', 'Bearer ' + tokens[user.username]);
+    let responseFood = await request.put(`/api/v2/food/${idFood[0]}`).send(editFood).set('Authorization', 'Bearer ' + tokens[user.username]);
+    let responseClothes = await request.put(`/api/v2/clothes/${idClothes[0]}`).send(editShirt).set('Authorization', 'Bearer ' + tokens[user.username]);
                
     //asert
     if ((user.role === 'admin') || (user.role === 'editor') ){ 
@@ -174,8 +174,8 @@ let shirt = {
         console.log(responseClothes.body.error);
 
        }
-      responseFood = await request.put(`/api/v2/food/${idFood}`).send(food).set('Authorization', 'Bearer ' + tokens[user.username]);
-      responseClothes = await request.put(`/api/v2/clothes/${idClothes}`).send(shirt).set('Authorization', 'Bearer ' + tokens[user.username]);
+      responseFood = await request.put(`/api/v2/food/${idFood[0]}`).send(food).set('Authorization', 'Bearer ' + tokens[user.username]);
+      responseClothes = await request.put(`/api/v2/clothes/${idClothes[0]}`).send(shirt).set('Authorization', 'Bearer ' + tokens[user.username]);
                   
        //asert
 
@@ -184,8 +184,8 @@ let shirt = {
     it('should delete the clothes/food item(s)',async()=>{
     console.log(user.username);
 
-    let responseFood  = await request.delete(`/api/v2/food/${idFood}`).set('Authorization', 'Bearer ' + tokens[user.username]) ;
-    let responseClothes  = await request.delete(`/api/v2/clothes/${idClothes}`).set('Authorization', 'Bearer ' + tokens[user.username]) ;
+    let responseFood  = await request.delete(`/api/v2/food/${idFood[0]}`).set('Authorization', 'Bearer ' + tokens[user.username]) ;
+    let responseClothes  = await request.delete(`/api/v2/clothes/${idClothes[0]}`).set('Authorization', 'Bearer ' + tokens[user.username]) ;
 
     if ((user.role === 'admin') ){ 
     expect(responseFood.status).toEqual(200);
